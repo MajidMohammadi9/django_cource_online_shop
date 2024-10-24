8c038841397dd4f329320eb7fca268e7de894e94
Majid Mohammadi <majidmohammadi7@hotmail.com>
2024-10-24 20:05:53 +0400

    add email filed to CustomUserCreationForm

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm,CustomUserChangeForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    add_form=CustomUserCreationForm
-    form=CustomUserChangeForm
-    model=CustomUser
-    list_display=('email','username')
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'username')
 
+    # if you want to add custom fileld to admin forms, use 'fieldsets' for change and 'add_fieldsets' for add user or creation user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {
+            "fields": (
+                'email',
+            ),
+        }),
+    )
+   
