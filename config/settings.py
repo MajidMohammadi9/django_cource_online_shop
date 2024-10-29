150f598e368f1f8aed0e3a7a560986a3c8c648f0
Majid Mohammadi <majidmohammadi7@hotmail.com>
2024-10-29 10:23:08 +0400

    change some accounts templates

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,13 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST="smtp.gmail.com"
+# EMAIL_USE_TLS="True"
+# EMAIL_PORT="587"
+# EMAIL_HOST_USER="your_account@gmail.com"
+# EMAIL_HOST_PASSWORD="your_password"
+
 
 
 # Internationalization
