ffeb73d2b0d22e2fea724b0cdf624d6ca3509bd7
Majid Mohammadi <majidmohammadi7@hotmail.com>
2024-11-01 19:54:24 +0400

    add product app model

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Products
+
+class ProductListView(generic.ListView):
+    queryset=Products.objects.filter(active=True)
+    template_name='products/product_list.html'
+    context_object_name='products'
+
+class ProductDetailView(generic.DetailView):
+    model=Products
+    template_name='products/product_detail.html'
+    context_object_name='product'
