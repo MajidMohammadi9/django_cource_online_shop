1efacd6dc0fbe3c7baf930a0a42e4de2f201cd77
Majid Mohammadi <majidmohammadi7@hotmail.com>
2024-11-26 22:23:36 +0400

    add product image field

--- cart/views.py
+++ cart/views.py
@@ -22,10 +22,15 @@ def add_to_cart_view(request,product_id):
               cleaned_data=form.cleaned_data
               quantity=cleaned_data['quantity']
               cart.add(product,quantity,replace_current_quantity=cleaned_data['inplace'])
-    return redirect('cart:cart_detail')
+
+    referrer = request.META.get('HTTP_REFERER') # This determine which page 'add to cart' was clicked from, so it redirects to that pages.
+    return redirect(referrer)
+    # return redirect('cart:cart_detail')
 
 def remove_from_cart(request,product_id):
       cart=Cart(request)
       product=get_object_or_404(Product,id=product_id)
       cart.remove(product)
-      return redirect('cart:cart_detail')
+      referrer = request.META.get('HTTP_REFERER')
+      return redirect(referrer)
+    # return redirect('cart:cart_detail')
